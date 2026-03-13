acaaea692affbbc69638cd1e2315ae0aa29f31ae
Patrick Neumann <patrick.neumann@giata.de>
2026-03-13 14:38:29 +0100

    Give me some space

--- html/scripts.js
+++ html/scripts.js
@@ -70,7 +70,7 @@ async function fetchBlockData() {
             ? `${strings.filtered_by || 'Filtered by'}: ${filter}` 
             : strings.automated_filter;
 
-        reasonEl.innerText += '\nRule:' + data.rule
+        reasonEl.innerText += '\nRule: ' + data.rule
     } catch (error) {
         // Fehler-Texte aus der strings.json
         console.log(error);
